a18967e423eb1ad688d0c958453d6b4a40fd6895
Timelord828 <64015360+Timelord828@users.noreply.github.com>
2020-04-27 00:05:14 +0300

    Третья лаба

--- 3 laba/src/Location.java
+++ 3 laba/src/Location.java
@@ -1,27 +1,18 @@
 import java.util.Objects;
-/**
- * This class represents a specific location in a 2D map.  Coordinates are
- * integer values.
- **/
-public class Location
-{
-    /** X coordinate of this location. **/
+/*This class represents a specific location in a 2D map.
+  Coordinates are integer values.*/
+public class Location {
+    //X coordinate of this location.
     public int xCoord;
-
-    /** Y coordinate of this location. **/
+    //Y coordinate of this location.
     public int yCoord;
-
-
-    /** Creates a new location with the specified integer coordinates. **/
-    public Location(int x, int y)
-    {
+    //Creates a new location with the specified integer coordinates.
+    public Location(int x, int y) {
         xCoord = x;
         yCoord = y;
     }
-
-    /** Creates a new location with coordinates (0, 0). **/
-    public Location()
-    {
+    //Creates a new location with coordinates (0, 0).
+    public Location() {
         this(0, 0);
     }
     public boolean equals(Object obj) {
@@ -35,5 +26,4 @@ public class Location
     public int hashCode() {
         return Objects.hash(xCoord, yCoord);
     }
-
 }
